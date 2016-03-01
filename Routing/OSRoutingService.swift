9547fb0983fd24de57b65d76d921ee2ddde74871
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-03-01 11:20:20 +0000

    Add objective-c compatible outline

---
+++ Routing/OSRoutingService.swift
@@ -0,0 +1,100 @@
+//
+//  OSRoutingService.swift
+//  Routing
+//
+//  Created by Dave Hardiman on 01/03/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+import Foundation
+
+/**
+ *  Objective-C compatible wrapper for RoutingService
+ */
+@objc(OSRoutingService)
+public class OSRoutingService: NSObject {
+    private let routingService: RoutingService
+
+    /**
+     Initialiser
+
+     - parameter vehicleType: The vehicle type to use
+     - parameter crs:         The Coordinate Reference System to use, or nil to use the default, EPSG:3857
+     */
+    public init(vehicleType: String, crs: String?) {
+        guard let vehicle = VehicleType(rawValue: vehicleType) else {
+            fatalError("Invalid vehicle type")
+        }
+        if crs == nil {
+            routingService = RoutingService(vehicleType: vehicle)
+        } else {
+            guard let coordSys = CoordinateReferenceSystem(rawValue: crs!) else {
+                fatalError("Invalid coordinate reference system")
+            }
+            routingService = RoutingService(vehicleType: vehicle, crs: coordSys)
+        }
+        super.init()
+    }
+
+    /**
+     Create route between the specified points
+
+     - parameter points:     The points to route between
+     - parameter completion: The completion handler to call
+     */
+    public func routeBetweenPoints(points: [Point], completion: (Route?, NSError?) -> Void) {
+
+    }
+}
+
+// MARK: - Vehicle and CRS types
+public extension OSRoutingService {
+    /**
+     Car vehicle type
+     */
+    public static func carVehicleType() -> String {
+        return VehicleType.Car.rawValue
+    }
+
+    /**
+     EMV vehicle type
+     */
+    public static func emvVehicleType() -> String {
+        return VehicleType.Emv.rawValue
+    }
+
+    /**
+     Foot vehicle type
+     */
+    public static func footVehicleType() -> String {
+        return VehicleType.Foot.rawValue
+    }
+
+    /**
+     MTB vehicle type
+     */
+    public static func mtbVehicleType() -> String {
+        return VehicleType.MTB.rawValue
+    }
+
+    /**
+     BNG CRS
+     */
+    public static func bngCRS() -> String {
+        return CoordinateReferenceSystem.BNG.rawValue;
+    }
+
+    /**
+     EPSG:27700
+     */
+    public static func epsg27700() -> String {
+        return CoordinateReferenceSystem.EPSG_27700.rawValue;
+    }
+
+    /**
+     EPSG:3857
+     */
+    public static func epsg3857() -> String {
+        return CoordinateReferenceSystem.EPSG_3857.rawValue;
+    }
+}
