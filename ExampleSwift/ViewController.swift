8ee2c0dccfb4a134077759abe344bf87d5773d18
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-03-03 11:39:33 +0000

    Add swift example project

---
+++ ExampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleSwift
+//
+//  Created by Dave Hardiman on 03/03/2016.
+//  Copyright © 2016 Ordnance Survey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
