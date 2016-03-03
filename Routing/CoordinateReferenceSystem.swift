e8ebcf42697dab9728036f75aa693cf97a48010b
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-03-03 13:07:44 +0000

    Expected value for graphopper is wgs84

--- Routing/CoordinateReferenceSystem.swift
+++ Routing/CoordinateReferenceSystem.swift
@@ -14,7 +14,7 @@ import Foundation
 public enum CoordinateReferenceSystem: String {
     case BNG = "bng"
     case EPSG_27700 = "EPSG:27700"
-    case WGS_84 = "WGS:84"
+    case WGS_84 = "wgs84"
     case EPSG_4326 = "EPSG:4386"
     case EPSG_3857 = "EPSG:3857"
     case EPSG_4258 = "EPSG:4258"
