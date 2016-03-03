5e7dc0a2debfd3d29db2e97faf92613161403e2c
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-03-03 09:24:52 +0000

    Add documentation for error values

--- Routing/OSRoutingError.h
+++ Routing/OSRoutingError.h
@@ -8,17 +8,50 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ *  Error domain for use with this framework
+ */
 extern NSString *const OSRoutingErrorDomain;
 
 NS_ENUM(NSInteger){
+    /**
+     *  At least 2 points are required for routing
+     */
     OSRoutingErrorTooFewPoints,
+    /**
+     *  No data was received from the server
+     */
     OSRoutingErrorNoDataReceived,
+    /**
+     *  Parsing the JSON response failed
+     */
     OSRoutingErrorFailedToParseJSON,
+    /**
+     *  The bounding box received is invalid
+     */
     OSRoutingErrorInvalidBoundingBox,
+    /**
+     *  There are no instructions in the response
+     */
     OSRoutingErrorMissingInstructions,
+    /**
+     *  There are no coordinates in the response
+     */
     OSRoutingErrorMissingCoordinates,
+    /**
+     *  Request is unauthorised. Check your API key is correct
+     */
     OSRoutingErrorUnauthorised,
+    /**
+     *  Invalid request made. See the localised description for the problem
+     */
     OSRoutingErrorBadRequest,
+    /**
+     *  Server error. See the localised description for the problem
+     */
     OSRoutingErrorServerError,
+    /**
+     *  Unknown error occurred
+     */
     OSRoutingErrorUnknown,
 };
