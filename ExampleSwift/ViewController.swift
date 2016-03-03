f17d3201909a89360a9271f171ca565aa8c16975
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-03-03 11:47:52 +0000

    Add points array to store tapped points

--- ExampleSwift/ViewController.swift
+++ ExampleSwift/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import MapKit
 import Routing
+import CoreLocation
 
 class ViewController: UIViewController {
 
@@ -21,6 +22,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var mapView: MKMapView!
 
     var routingService: RoutingService!
+
+    lazy var tappedPoints = [CLLocationCoordinate2D]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
