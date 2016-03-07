b0e660f1bd4dc00e2b71e5981a9dfffe88392d37
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2016-03-07 09:27:09 +0000

    Add tapped points

--- ExampleObjc/ViewController.m
+++ ExampleObjc/ViewController.m
@@ -13,6 +13,7 @@
 @interface ViewController ()<MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (strong, nonatomic) OSRoutingService *routingService;
+@property (strong, nonatomic) NSMutableArray *tappedPoints;
 @end
 
 @implementation ViewController
@@ -27,6 +28,24 @@
     [super viewDidLoad];
     self.routingService = [[OSRoutingService alloc] initWithApiKey:self.apiKey vehicleType:[OSRoutingService carVehicleType] crs:nil];
     self.mapView.delegate = self;
+    self.tappedPoints = NSMutableArray.array;
+
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(mapTapped:)];
+    [self.mapView addGestureRecognizer:tap];
+}
+
+#pragma mark - Map Tapped
+- (void)mapTapped:(UITapGestureRecognizer *)tap {
+    if (tap.state != UIGestureRecognizerStateEnded) {
+        return;
+    }
+    CGPoint touchPoint = [tap locationInView:self.mapView];
+    CLLocationCoordinate2D touchCoordinate = [self.mapView convertPoint:touchPoint toCoordinateFromView:self.mapView];
+    MKPointAnnotation *pointAnnotation = [[MKPointAnnotation alloc] init];
+    pointAnnotation.coordinate = touchCoordinate;
+    [self.mapView addAnnotation:pointAnnotation];
+    OSPoint *point = [[OSPoint alloc] initWithX:touchCoordinate.latitude y:touchCoordinate.longitude];
+    [self.tappedPoints addObject:point];
 }
 
 @end
